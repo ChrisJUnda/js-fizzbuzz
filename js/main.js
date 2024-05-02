4c6c796bf188d5cb680841a2137961a1069cb3b2
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-02 16:37:04 +0200

    [Ex finished]

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict'
+console.log('Buona giornata a tutti')
+
+for (let counter = 1; counter <= 100; counter++) {
+    console.log(counter);
+   if(counter % 3 === 0 && counter % 5 === 0){
+    console.log('FizzBuzz');
+} else if(counter % 3 === 0){
+    console.log('Fizz');
+} else if(counter % 5 === 0){
+    console.log('Buzz');
+}
+console.log('--');
+}
+
+
+
+
